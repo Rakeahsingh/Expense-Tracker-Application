9914b2c2465c1e609ff06ceb9fdb6be0ebf2fe13
Rakeahsingh <bca02121502019@gmail.com>
2024-04-06 20:44:13 +0530

    hello

--- app/src/main/java/com/rkcoding/expensetrackerapplication/app_features/presentation/addTransactionScreen/AddTransactionScreen.kt
+++ app/src/main/java/com/rkcoding/expensetrackerapplication/app_features/presentation/addTransactionScreen/AddTransactionScreen.kt
@@ -1,12 +1,80 @@
 package com.rkcoding.expensetrackerapplication.app_features.presentation.addTransactionScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.rkcoding.expensetrackerapplication.core.Constant
+import com.rkcoding.expensetrackerapplication.ui.theme.incomeGradient
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTransactionScreen(
     navController: NavController
 ) {
-    Text(text = "Add Transaction Screen")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Expense Transaction") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "back arrow icon"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription ="check icon",
+                            modifier = Modifier.size(45.dp)
+                                .background(incomeGradient, CircleShape)
+                        )
+                    }
+
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Cancel,
+                            contentDescription ="check icon",
+                            modifier = Modifier.size(45.dp)
+                                .background(incomeGradient, CircleShape)
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = it.calculateTopPadding())
+        ) {
+
+        }
+    }
 }
---
+++ app/src/main/java/com/rkcoding/expensetrackerapplication/utils/Resources.kt
@@ -0,0 +1,9 @@
+package com.rkcoding.expensetrackerapplication.utils
+
+sealed class Resources<T>(val data: T? = null, val message: String? = null) {
+
+    class Success<T>(data: T): Resources<T>(data)
+    class Error<T>(data: T? = null, message: String): Resources<T>(data, message)
+    class Loading<T>(data: T? = null): Resources<T>(data = null)
+
+}
